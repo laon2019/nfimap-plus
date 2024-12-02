febb29964f779bd27fa1635a5e6a0298a56b5893
laon2019 <laon2019@nate.com>
2024-12-02 10:30:37 +0900

    feat : 프로세스

--- src/pages/Nfiti/components/Process.tsx
+++ src/pages/Nfiti/components/Process.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, VStack, Image, Text, HStack, Button } from "@chakra-ui/react";
+import { Box, VStack, Text, HStack, Button } from "@chakra-ui/react";
 
 interface Question {
   id: number;
@@ -18,8 +18,8 @@ const Process: React.FC<ProcessProps> = ({ currentQuestionIndex, handleAnswer, q
   const progress = ((currentQuestionIndex + 1) / questions.length) * 100;
 
   return (
-    <Box p={4}>
-      <VStack spacing={6} align="stretch">
+    <Box height="calc(100vh - 68px)" p={4}>
+      <VStack spacing={6} align="stretch" height="100%">
         <Box position="relative" h="20px" bg="gray.100" borderRadius="full">
           <Box
             position="absolute"
@@ -31,7 +31,8 @@ const Process: React.FC<ProcessProps> = ({ currentQuestionIndex, handleAnswer, q
             borderRadius="full"
             transition="width 0.3s ease-in-out"
           >
-            <Image
+            <Box
+              as="img"
               src="/image/nfiti/loading/loading-gif.gif"
               alt="로딩 이미지"
               position="absolute"
@@ -43,16 +44,25 @@ const Process: React.FC<ProcessProps> = ({ currentQuestionIndex, handleAnswer, q
             />
           </Box>
         </Box>
-        <Box p={4} borderWidth={1} borderRadius="lg">
-          <VStack spacing={4}>
-            <Image
-              src={currentQuestion.image}
-              alt={`질문 ${currentQuestion.id} 이미지`}
-              width="100%"
-              height="600px"
-              objectFit="cover"
-              borderRadius="md"
-            />
+        <Box 
+          p={4} 
+          borderWidth={1} 
+          borderRadius="lg"
+          flex="1"
+          backgroundImage={`url(${currentQuestion.image})`}
+          backgroundSize="cover"
+          backgroundPosition="center"
+          position="relative"
+        >
+          <VStack 
+            spacing={4}
+            position="absolute"
+            bottom="40px"
+            left="0"
+            right="0"
+            padding="20px"
+            background="rgba(255, 255, 255, 0.9)"
+          >
             <Text fontSize="lg">{currentQuestion.text}</Text>
             <HStack spacing={4} justifyContent="center">
               <Button colorScheme="green" onClick={() => handleAnswer(true)}>
